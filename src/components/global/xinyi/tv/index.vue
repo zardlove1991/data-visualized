981c24f0968308c2aaebc40da7edfed0610d5989
lijun <lijun@hoge.cn>
2019-09-23 14:35:31 +0800

    电视监看

---
+++ src/components/global/xinyi/tv/index.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="xy-tv" id="xy-tv">
+    <div class="tv-wrap sys-flex sys-vertical" :class="[{'warp-bg' : showDefault}]" :style="defineBg()">
+      <div class="tv-title">{{tvTitle}}</div>
+      <div class="tv-main sys-flex sys-flex-center flex-justify-between ">
+        <div v-for="(v,k) in tvList" :key="k" class="tv-list">
+          <video-player
+            class="vjs-custom-skin"
+            ref="videoPlayer"
+            :options="v.playerOpt"
+            :playsinline="true"
+            customEventName="customstatechangedeventname"
+          ></video-player>
+          <div class="tv-name">{{v.name}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import 'video.js/dist/video-js.css'
+import { videoPlayer } from 'vue-video-player'
+import 'videojs-contrib-hls'
+import { getTvList } from '@/servers/xinyi'
+export default {
+  name: 'tv',
+  data () {
+    return {
+      tvTitle: '电视监看',
+      tvList: []
+    }
+  },
+  components: {
+    videoPlayer
+  },
+  created () {
+    this.getTvList()
+  },
+  mounted () {
+    this.setFontsize('xy-tv')
+  },
+  methods: {
+    getTvList () {
+      getTvList().then(res => {
+        if (res && res.data && res.data.length) {
+          let data = res.data
+          data.forEach(value => {
+            value.playerOpt = {
+              autoplay: true, // 自动播放
+              controls: true, // 是否显示控制栏
+              muted: true,
+              fluid: true,
+              width: 820,
+              // techOrder: ['flash', 'html5'], // 兼容顺序
+              sourceOrder: true,
+              flash: { hls: { withCredentials: false } },
+              html5: { hls: { withCredentials: false } },
+              sources: [
+                {
+                  withCredentials: false,
+                  type: 'application/x-mpegURL',
+                  src: value.m3u8
+                }
+              ],
+              poster:
+                value.snap.host +
+                value.snap.dir +
+                value.snap.filepath +
+                value.snap.filename, // 播放器默认图片
+              controlBar: {
+                // 配置控制栏
+                timeDivider: false, // 时间分割线
+                durationDisplay: false, // 总时间
+                progressControl: true, // 进度条
+                customControlSpacer: true, // 未知
+                fullscreenToggle: true // 全屏
+              }
+            }
+          })
+          this.tvList = data.slice(0, 4)
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import 'src/styles/index.scss';
+.xy-tv {
+  width: 100%;
+  padding: 0.25vh 0.25vw;
+  .tv-wrap {
+    width: 100%;
+    height: 100%;
+    padding: px1em(17px) px1em(11px);
+  }
+  .warp-bg {
+    background: url('./assets/bg.png') no-repeat center;
+    background-size: 100% 100%;
+  }
+  .tv-title {
+    text-align: left;
+    height: 20%;
+    font-size: px1em(22px);
+    color: #d6e6ff;
+  }
+  .tv-main {
+    width: 100%;
+    height: 80%;
+    background: url("./assets/back.png") no-repeat center;
+    background-size: 100%;
+    padding: 0 0.4em;
+    .tv-list {
+      width: 8em;
+      // height: 5.6em;
+      .vjs-custom-skin {
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+      }
+      .tv-name {
+        color: #00fff6;
+        font-size: 0.36em;
+        text-align: center;
+        margin-top: 0.15em;
+      }
+    }
+  }
+}
+</style>
