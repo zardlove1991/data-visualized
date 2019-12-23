faee52d4fd2035e487e7ad86bdd5f12952e7fd2c
lxp <lxp@aaa.local>
2019-12-23 10:42:39 +0800

    lxp:临时保存

--- src/components/global/common01/clueGather/index.vue
+++ src/components/global/common01/clueGather/index.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>
-    1234567890
+  <div class="common-cluegather">
+    <div class="cluegather-wrap">
+      <div class="wrap-content">
+
+      </div>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: 'clueGather',
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "src/styles/index.scss";
+.common-cluegather {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding: pxrem(40px);
+  .cluegather-wrap {
+    width: 100%;
+    height: 100%;
+  }
+}
+</style>
