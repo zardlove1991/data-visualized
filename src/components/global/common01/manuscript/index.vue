375f53b5e097f20b20cda0ae8f2705afb4320086
BaoHan <baohan@hogecloud.com>
2019-12-23 10:29:44 +0800

    组件添加

---
+++ src/components/global/common01/manuscript/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    稿件生产
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
--- src/components/global/index.js
+++ src/components/global/index.js
@@ -74,3 +74,7 @@ export const jiangningcomponentReport = getComponent('jiangning/report')
 export const jiangningcomponentProject = getComponent('jiangning/project')
 export const jiangningcomponentPublish = getComponent('jiangning/publish')
 export const jiangningcomponentNews = getComponent('jiangning/news')
+
+/* 标准套系01 */
+export const common01componentManuscript = getComponent('common01/manuscript')
+export const common01componentMessage = getComponent('common01/message')
