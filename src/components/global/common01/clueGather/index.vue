8e0819dc6d9616b6f6d67a4872238a5d5a0fb510
lxp <lxp@aaa.local>
2019-12-27 09:38:15 +0800

    lxp:样式修改

--- src/components/global/common01/clueGather/index.vue
+++ src/components/global/common01/clueGather/index.vue
@@ -63,7 +63,8 @@ export default {
 }
 </script>
 <style lang="scss">
-@import '~@/styles/index.scss';
+@import "~@/styles/index.scss";
+@import '../style/index.scss';
 .common01-cluegather {
   width: 100%;
   height: 100%;
@@ -91,26 +92,5 @@ export default {
       }
     }
   }
-}
-.common01-border {
-  width: 100%;
-  height: 100%;
-  background: url("../../../../assets/common/common01Border.png") no-repeat center;
-  background-size: 100% 100%;
-  position: relative;
-}
-.common01-title {
-  font-size: pxrem(58px);
-  font-weight: 500;
-  text-shadow: 0 pxrem(16px) pxrem(16px) rgba(0, 222, 255, 0.2);
-  position: absolute;
-  top: pxrem(54px);
-  left: pxrem(72px);
-}
-.common01-ft40 {
-  font-size: pxrem(40px);
-}
-.common01-ft32 {
-  font-size: pxrem(32px);
 }
 </style>
