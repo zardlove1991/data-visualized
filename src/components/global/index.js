7e701c24ddd777a1b69b6e46fc4f357eaa60e718
BaoHan <baohan@hogecloud.com>
2020-01-17 18:20:37 +0800

    标题

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -79,6 +79,7 @@ export const jiangningcomponentPublish = getComponent('jiangning/publish')
 export const jiangningcomponentNews = getComponent('jiangning/news')
 
 /* 标准套系01 */
+export const common01componentTitle = getComponent('common01/title') // 标题
 export const common01componentClueGather = getComponent('common01/clueGather')
 export const common01componentManuscript = getComponent('common01/manuscript')
 export const common01componentMessage = getComponent('common01/message') // 消息汇总
