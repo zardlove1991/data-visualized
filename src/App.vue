0cff0ae4a4c79dc112ebf46b847b16da19c3fc69
zhangfangyuan@hoge.cn <zhangfangyuan@hoge.cn>
2020-03-31 15:16:12 +0800

    添加ico 修改页签标题

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,14 @@
 export default {
   name: 'App',
   created () {
+  },
+  watch: {
+    $route: function (v) {
+      // 盐湖大屏更换标题
+      if (/ZTk3ZjE1Ym/.test(location.href)) {
+        document.title = '盐湖区新时代文明实践中心'
+      }
+    }
   }
 }
 </script>
