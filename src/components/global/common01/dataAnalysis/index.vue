02f8509365e2bb5316e375e2fc915ed9b3918133
郎孟捷 <lmj@langmengjiedeMac-mini.local>
2020-06-06 16:30:32 +0800

    盐城大屏开发

--- src/components/global/common01/dataAnalysis/index.vue
+++ src/components/global/common01/dataAnalysis/index.vue
@@ -2,9 +2,60 @@
   <div class="common01-report">
     <div class="report-wrap common01-border">
        <div class="common01-title">{{viewAttr.header || '舆情分析'}}</div>
-       <div class="geographical-distribution chart-box">
-          <chart :options="geoOpt" :autoResize="true"></chart>
-        </div>
+       <div class="sweiper-box">
+           <!-- <img src="./assets/active-bg.png" alt=""> -->
+             <el-carousel :autoplay="false" :interval="3000" type="card" height= "100px">
+                <el-carousel-item v-for="item in list" :key="item">
+                    <p class="slide-title">{{item}}</p>
+                </el-carousel-item>
+            </el-carousel>
+       </div>
+       <div class="bottom-box">
+           <div class="qinggan-box">
+               <div style="display:flex; flex-direction: row;align-items: center;">
+                    <img class="title-logo" src="./assets/title.png" alt="">
+                    <p class="bottom-title">情感分析</p>
+               </div>
+               <div class="qinggan-item-box"> 
+                    <div class="qinggan-item">
+                        <p class="qinggan-title">正面：615 </p>
+                        <div style="display:flex; flex-direction: row;align-items: center;">
+                            <div class="all-percent">
+                                <div class="item-percent green"></div>
+                            </div>
+                            <span class="percent-num">25%</span>
+                        </div>
+                    </div>
+                       <div class="qinggan-item">
+                        <p class="qinggan-title">中立：1320 </p>
+                        <div style="display:flex; flex-direction: row;align-items: center;">
+                            <div class="all-percent">
+                                <div class="item-percent yellow"></div>
+                            </div>
+                            <span class="percent-num">62%</span>
+                        </div>
+                    </div>
+                       <div class="qinggan-item">
+                        <p class="qinggan-title">负面：446 </p>
+                        <div style="display:flex; flex-direction: row;align-items: center;">
+                            <div class="all-percent">
+                                <div class="item-percent red"></div>
+                            </div>
+                            <span class="percent-num">13%</span>
+                        </div>
+                    </div>
+               </div>
+               
+           </div>
+            <div class="geographical-distribution chart-box">
+                <div style="display:flex; flex-direction: row;align-items: center;">
+                    <img class="title-logo" src="./assets/title.png" alt="">
+                    <p class="bottom-title">情感分析</p>
+               </div>
+                <chart :options="geoOpt" :autoResize="true"></chart>
+            </div>
+       </div>
+     
     </div>
   </div>
 </template>
@@ -20,14 +71,22 @@ import 'echarts/lib/component/grid'
 import 'echarts/lib/component/legend'
 import 'echarts/lib/component/tooltip'
 import 'echarts/lib/component/visualMap'
+import { Carousel, CarouselItem } from 'element-ui'
 export default {
   name: 'dataAnalysis',
   components: {
-    chart: echarts
+    chart: echarts,
+    Carousel,
+    CarouselItem
   },
   data () {
     return {
-      proportion: 1
+      proportion: 1,
+      list: [
+        '什刹海冰场正式开放 市民排队入场 冰面游人如织',
+        '奥术大师大所多奥术大师大',
+        '奥术大师大多双方各但是广东省防守打法'
+      ]
     }
   },
   created () {
@@ -53,7 +112,7 @@ export default {
           {
             type: 'category',
             axisTick: {
-              show: false,
+              show: true,
               alignWithLabel: true
             },
             axisLabel: {
@@ -61,7 +120,7 @@ export default {
               margin: 15,
               textStyle: {
                 color: '#fff',
-                fontSize: 30 // 更改坐标轴文字大小
+                fontSize: 24 // 更改坐标轴文字大小
               }
             },
             axisLine: {
@@ -71,7 +130,7 @@ export default {
                 type: 'solid'
               }
             },
-            data: ['江苏', '江苏', '江苏', '江苏', '江苏', '江苏', '江苏']
+            data: ['吉林', '安徽', '西安', '江苏', '北京', '广州', '湖南']
           }
         ],
         yAxis: [
@@ -161,6 +220,124 @@ export default {
             height: 100%;
         }
      }
+    .sweiper-box{
+        width: 100%;
+        height: pxrem(120px);
+        margin: pxrem(80px) 0;
+        img{
+            width: pxrem(1300px);
+            height: pxrem(120px);
+        }
+    }
+    .bottom-box{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        .bottom-title{
+            text-align: left;
+            font-size: pxrem(44px);
+            font-family:PingFang SC;
+            font-weight:500;
+            color:rgba(72,241,223,1);
+            margin-left: pxrem(20px);;
+        }
+        .qinggan-item-box{
+            padding-top: pxrem(25px);
+            .qinggan-item{
+                height: pxrem(150px);
+            }
+            display: flex;
+            flex-direction: column;
+            .qinggan-title{
+                text-align: left;
+                font-size: pxrem(36px);
+                font-family:PingFang SC;
+                font-weight:500;
+                color:rgba(254,254,254,1);
+                margin: pxrem(20px) 0;
+            }
+            .all-percent{
+                position: relative;
+                left: 0;
+                width: pxrem(440px);
+                height: pxrem(10px);
+                background-color:#21479A;
+                border-radius: pxrem(5px);
+                .green{
+                    width: pxrem(204px);
+                    background-color: #00BDA4;
+                }
+                .yellow{
+                    width: pxrem(300px);
+                    background-color: #C8C232;
+                }
+                .red{
+                    width: pxrem(160px);
+                    background-color: #D83E69;
+                }
+                .item-percent{
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    height: pxrem(10px);
+                    border-radius: pxrem(5px);
+                    &::after{
+                        width: pxrem(20px);
+                        height: pxrem(20px);
+                        border-radius: 50%;
+                    }
+                }
+            }
+            .percent-num{
+                font-size:pxrem(32px);
+                font-family:PingFang SC;
+                font-weight:400;
+                color:rgba(254,254,254,1);
+                margin-left: pxrem(20px);
+            }
+        }
+    }
   }
 }
+  .el-carousel__item h3 {
+    color: #475669;
+    font-size: 14px;
+    opacity: 0.75;
+    line-height: 200px;
+    margin: 0;
+  }
+  .slide-title{
+    font-size:pxrem(44px);
+    font-family:PingFang SC;
+    font-weight:500;
+    color:rgba(255,255,255,1);
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+.title-logo{
+    width: pxrem(40px);
+    height: pxrem(40px);
+}
+//   /deep/{
+.el-carousel__indicators--outside{
+    display: none;
+}
+.el-carousel__item--card{
+    // background-image: url("./assets/no-active.png");
+    display: flex;
+    align-items: center;
+    background-color: #013D99;
+    padding: 0 pxrem(20px);
+}
+.el-carousel__item--card.is-active{
+    // background-image: url("./assets/active-bg.png");
+    display: flex;
+    align-items: center;
+    background-color: #2634C3;
+    padding: 0 pxrem(20px);
+}
+//   }
 </style>
--- src/components/global/common01/yanchengGather/index.vue
+++ src/components/global/common01/yanchengGather/index.vue
@@ -1,7 +1,83 @@
 <template>
   <div class="common01-report">
-    <div class="report-wrap common01-border">
-      盐城聚合信息
+    <div class="report-wrap">
+        <div class="rong-box">
+            <img src="./assets/image.jpeg" alt="">
+        </div>
+        <div class="bo-box">
+            <img class="bo-logo" src="./assets/hhfb.png" alt="">
+            <!-- <img class="bo2" src="./assets/bo2.png" alt=""> -->
+            <p>盐城广播</p>
+        </div>
+        <div class="bao-box"></div>
+        <div class="wang-box">
+            <div class="wang-list">
+                <div class="wang-list-item">
+                    马化腾已经对库克动手了，网友 们表示：就看马云的了
+                </div>
+                <div class="wang-list-item">
+                     马化腾已经对库克动手了，网友 们表示：就看马云的了
+                </div>
+                <div class="wang-list-item">
+                     马化腾已经对库克动手了，网友 们表示：就看马云的了
+                </div>
+                <div class="wang-list-item">
+                     马化腾已经对库克动手了，网友 们表示：就看马云的了
+                </div>
+            </div>
+        </div>
+        <div class="wei-box">
+            <div class="wei-list">
+                <div class="wei-list-item">
+                    <img src="./assets/hhfb.png" alt="">
+                    <div>
+                        <p class="wei-title">黄海街道“娃娃趣说到...</p>
+                        <p class="wei-tag">黄海街道</p>
+                    </div>
+                </div>
+                  <div class="wei-list-item">
+                    <img src="./assets/hhfb.png" alt="">
+                    <div>
+                        <p class="wei-title">黄海街道“娃娃趣说到...</p>
+                        <p class="wei-tag">黄海街道</p>
+                    </div>
+                </div>
+                <div class="wei-list-item">
+                    <img src="./assets/qnzx.png" alt="">
+                    <div>
+                        <p class="wei-title">黄海街道“娃娃趣说到...</p>
+                        <p class="wei-tag">黄海街道</p>
+                    </div>
+                </div>
+                <div class="wei-list-item">
+                    <img src="./assets/cnsb.png" alt="">
+                    <div>
+                        <p class="wei-title">黄海街道“娃娃趣说到...</p>
+                        <p class="wei-tag">黄海街道</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="duan-box">
+            <div class="duan-list">
+                <div class="duan-list-item">
+                    <p class="duan-title">俄媒：中国对美博弈可能“不战...</p>
+                    <p class="duan-tag">青年网  06-02 13:09</p>
+                </div>
+                <div class="duan-list-item">
+                    <p class="duan-title">俄媒：中国对美博弈可能“不战...</p>
+                    <p class="duan-tag">青年网  06-02 13:09</p>
+                </div>
+                <div class="duan-list-item">
+                    <p class="duan-title">俄媒：中国对美博弈可能“不战...</p>
+                    <p class="duan-tag">青年网  06-02 13:09</p>
+                </div>
+                <div class="duan-list-item">
+                    <p class="duan-title">俄媒：中国对美博弈可能“不战...</p>
+                    <p class="duan-tag">青年网  06-02 13:09</p>
+                </div>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -24,6 +100,145 @@ export default {
 .common01-report {
   width: 100%;
   height: 100%;
-  padding: pxrem(40px);
+  padding: pxrem(20px) pxrem(40px) pxrem(40px);
+  .report-wrap{
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+        .rong-box{
+            width: pxrem(1230px);
+            height: pxrem(1040px);
+            background: url("./assets/rong.png") no-repeat;
+            padding: pxrem(200px) pxrem(40px) pxrem(40px) pxrem(230px);
+            img{
+                width: 100%;
+                height: 100%;
+            }
+        }
+        .bo-box{
+            width: pxrem(910px);
+            height: pxrem(1040px);
+            background: url("./assets/bo1.png") no-repeat;
+            position: relative;
+            .bo-logo{
+                width: pxrem(150px);
+                height: pxrem(150px);
+                position: absolute;
+                top: 30%;
+                left: 52%;
+            }
+            .bo2{
+                width: 100%;
+                height: pxrem(265px);
+            }
+            p{
+                font-size:pxrem(70px);
+                font-family:PingFang SC;
+                font-weight:600;
+                text-align: center;
+                color:rgba(255,255,255,1);
+                position: absolute;
+                top: 48%;
+                left: 45%;
+            }
+        }
+        .bao-box{
+            width: pxrem(870px);
+            height: pxrem(1040px);
+            background: url("./assets/bao.png") no-repeat;
+        }
+        .wang-box{
+            width: pxrem(970px);
+            height: pxrem(1040px);
+            background: url("./assets/wang.png") no-repeat;
+            padding: pxrem(240px) pxrem(40px) pxrem(40px) pxrem(230px);
+            .wang-list{
+                width: 100%;
+                height: 100%;
+                .wang-list-item{
+                    padding: pxrem(20px);
+                    width:pxrem(660px);
+                    height:pxrem(146px);
+                    background:linear-gradient(180deg,rgba(26,118,255,0.24) 0%,rgba(0,110,252,0) 100%);
+                    margin-bottom: pxrem(50px);
+                    text-align: left;
+                    font-size: pxrem(40px);
+                    font-family:PingFang SC;
+                    font-weight:400;
+                    color:rgba(254,254,254,1);
+                }
+            }
+        }
+        .wei-box{
+            width: pxrem(950px);
+            height: pxrem(1040px);
+            background: url("./assets/wei.png") no-repeat;
+            padding: pxrem(240px) pxrem(40px) pxrem(40px) pxrem(230px);
+            .wei-list-item{
+                width:pxrem(660px);
+                height:pxrem(146px);
+                display: flex;
+                flex-direction: row;
+                margin-bottom: pxrem(50px);
+                img{
+                    width: pxrem(150px);
+                    height: pxrem(150px);
+                    margin-right: pxrem(20px);
+                }
+                .wei-title{
+                    font-size:pxrem(40px);
+                    font-family:PingFang SC;
+                    font-weight:400;
+                    text-align: left;
+                    color:rgba(255,255,255,1);
+                    margin-bottom: pxrem(20px);
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    overflow: hidden;
+                }
+                .wei-tag{
+                    font-size: pxrem(36px);
+                    font-family:PingFang SC;
+                    font-weight:400;
+                    color:rgba(0,198,255,1);
+                    text-align: left;
+                }
+            }
+        }
+        .duan-box{
+            width: pxrem(960px);
+            height: pxrem(1040px);
+            background: url("./assets/duan.png") no-repeat;
+            padding: pxrem(240px) pxrem(40px) pxrem(40px) pxrem(230px);
+            .duan-list-item{
+                width:pxrem(660px);
+                height:pxrem(146px);
+                margin-bottom: pxrem(50px);
+                border-bottom:2px dashed rgba(0,222,255,1);
+                    .duan-title{
+                    font-size:pxrem(40px);
+                    font-family:PingFang SC;
+                    font-weight:400;
+                    text-align: left;
+                    color:rgba(255,255,255,1);
+                    margin-bottom: pxrem(20px);
+                    text-overflow: ellipsis;
+                    white-space: nowrap;
+                    overflow: hidden;
+                }
+                .duan-tag{
+                    font-size: pxrem(36px);
+                    font-family:PingFang SC;
+                    font-weight:400;
+                    color:rgba(0,198,255,1);
+                    text-align: left;
+                }
+            }
+        }
+  }
 }
+
 </style>
