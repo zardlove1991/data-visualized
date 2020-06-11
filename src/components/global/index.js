41ebf863846b6c82240499476d7e69def29365e9
郎孟捷 <lmj@langmengjiedeMac-mini.local>
2020-06-11 15:23:39 +0800

    宾川标题添加

--- src/components/global/index.js
+++ src/components/global/index.js
@@ -134,3 +134,4 @@ export const common01componentMyYancheng = getComponent('common01/myYancheng') /
 export const common01componentLiveCamera = getComponent('common01/liveCamera') // 盐城直播摄像头
 export const common01componentYanchengGather = getComponent('common01/yanchengGather') // 盐城聚合信息
 export const common01componentYanchengScreen = getComponent('common01/yanchengScreen') // 盐城聚合信息
+export const common01componentTitle02 = getComponent('common01/title02') // 标题2
