3b6c2361334e48900a30865652e79f6a08b044cb
yxn <yexinnian@hoge.cn>
2020-08-27 12:14:49 +0800

    修改 报题展示2

--- src/components/global/common01/report2/index.vue
+++ src/components/global/common01/report2/index.vue
@@ -4,7 +4,7 @@
       <div class="common01-title" :style="setFontSize(63)">{{viewAttr.header || '报题展示'}}</div>
       <div class="wrap-content">
         <div class="item-list sys-flex sys-flex-center animated" v-for="(v, k) in dataList" :key="k" :class="{'flipInX' : v.title}" :style="{'animation-delay' : k/2+'s'}">
-          <div class="status common01-ft30" :class="{'one': v.audit_status === '0', 'two': v.audit_status === '1', 'three': v.audit_status === '2', 'four': v.audit_status === '4'}">{{v.audit_status === '0' ? '待审核' : v.audit_status === '1' ? '通过' : v.audit_status === '2' ? '打回' : '报审'}}</div>
+          <div class="status common01-ft30" :class="{'one': v.audit_status === '0', 'two': v.audit_status === '1', 'three': v.audit_status === '2', 'four': v.audit_status === '4'}">{{v.audit_status === '0' ? '待审核' : v.audit_status === '1' ? '已通过' : v.audit_status === '2' ? '已打回' : '报审'}}</div>
           <div class="title common01-ft40 overhidden" :style="setFontSize(50)">{{v.title}}</div>
           <div class="name common01-ft32 overhidden" :style="setFontSize(45)">{{v.project_user_name}}</div>
           <div class="time common01-ft32" :style="setFontSize(45)">{{v.update_time | dateFormat}}</div>
@@ -79,7 +79,7 @@ export default {
   height: 100%;
   padding: pxrem(40px);
   .report-wrap {
-    padding: pxrem(230px) pxrem(96px) pxrem(95px) pxrem(78px);
+    padding: pxrem(230px) pxrem(80px) pxrem(95px) pxrem(78px);
     color: #fff;
     .wrap-content {
       .item-list {
@@ -88,9 +88,9 @@ export default {
           margin-bottom: 0;
         }
         .status {
-          width: pxrem(200px);
+          width: pxrem(260px);
           height: pxrem(60px);
-          line-height: pxrem(60px);
+          line-height: pxrem(64px);
           border-radius: pxrem(2px);
           &.one {
             background-color: #0466FF;
@@ -107,11 +107,11 @@ export default {
         }
         .title {
           text-align: left;
-          width: 55%;
-          margin: 0 pxrem(135px) 0 pxrem(37px);
+          width: 72%;
+          margin: 0 pxrem(35px) 0 pxrem(20px);
         }
         .name {
-          width: 12%;
+          width: 27%;
           text-align: left;
         }
         .time {
