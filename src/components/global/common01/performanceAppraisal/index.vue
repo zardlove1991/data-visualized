99d4f817a308a49c9a6d90cc2a93860d2ba227a5
wanganqi <843348669@qq.com>
2021-03-17 17:51:51 +0800

    山西大屏 绩效考核 组件

---
+++ src/components/global/common01/performanceAppraisal/index.vue
@@ -0,0 +1,423 @@
+<template>
+  <div class="performanceAppraisal" id="performanceAppraisal">
+    <div class="performanceAppraisal-wrap">
+      <div id="scroll_div" class="scroll_div">
+        <div id="scroll_begin" class="scroll_begin">
+            <ul>
+              <li class="item">
+                <div class="item_top">
+                  <div class="touxiang">
+                    <img src="./assets/head.png"/>
+                    <p class="user_name">陈小1</p>
+                  </div>
+                  <div class="top_right">
+                    <div class="pro right_div">
+                      生产量<i>52</i>
+                    </div>
+                    <div class="dev right_div">
+                      发布量<i>37</i>
+                    </div>
+                  </div>
+                </div>
+                <div class="split_line"></div>
+                <div class="item_bottom">
+                  <div class="click bottom_div">
+                    <p class="num">10,230</p>
+                    <p class="tit">总点击量</p>
+                  </div>
+                  <div class="comment bottom_div">
+                    <p class="num">3,125</p>
+                    <p class="tit">总评论量</p>
+                  </div>
+                  <div class="zan bottom_div">
+                    <p class="num">7,265</p>
+                    <p class="tit">总点赞量</p>
+                  </div>
+                  <div class="share bottom_div">
+                    <p class="num">1,102</p>
+                    <p class="tit">总分享量</p>
+                  </div>
+                </div>
+              </li>
+              <li class="item">
+                <div class="item_top">
+                  <div class="touxiang">
+                    <img src="./assets/head.png"/>
+                    <p class="user_name">陈小2</p>
+                  </div>
+                  <div class="top_right">
+                    <div class="pro right_div">
+                      生产量<i>52</i>
+                    </div>
+                    <div class="dev right_div">
+                      发布量<i>37</i>
+                    </div>
+                  </div>
+                </div>
+                <div class="split_line"></div>
+                <div class="item_bottom">
+                  <div class="click bottom_div">
+                    <p class="num">10,230</p>
+                    <p class="tit">总点击量</p>
+                  </div>
+                  <div class="comment bottom_div">
+                    <p class="num">3,125</p>
+                    <p class="tit">总评论量</p>
+                  </div>
+                  <div class="zan bottom_div">
+                    <p class="num">7,265</p>
+                    <p class="tit">总点赞量</p>
+                  </div>
+                  <div class="share bottom_div">
+                    <p class="num">1,102</p>
+                    <p class="tit">总分享量</p>
+                  </div>
+                </div>
+              </li>
+              <li class="item">
+                <div class="item_top">
+                  <div class="touxiang">
+                    <img src="./assets/head.png"/>
+                    <p class="user_name">陈小3</p>
+                  </div>
+                  <div class="top_right">
+                    <div class="pro right_div">
+                      生产量<i>52</i>
+                    </div>
+                    <div class="dev right_div">
+                      发布量<i>37</i>
+                    </div>
+                  </div>
+                </div>
+                <div class="split_line"></div>
+                <div class="item_bottom">
+                  <div class="click bottom_div">
+                    <p class="num">10,230</p>
+                    <p class="tit">总点击量</p>
+                  </div>
+                  <div class="comment bottom_div">
+                    <p class="num">3,125</p>
+                    <p class="tit">总评论量</p>
+                  </div>
+                  <div class="zan bottom_div">
+                    <p class="num">7,265</p>
+                    <p class="tit">总点赞量</p>
+                  </div>
+                  <div class="share bottom_div">
+                    <p class="num">1,102</p>
+                    <p class="tit">总分享量</p>
+                  </div>
+                </div>
+              </li>
+              <li class="item">
+                <div class="item_top">
+                  <div class="touxiang">
+                    <img src="./assets/head.png"/>
+                    <p class="user_name">陈小4</p>
+                  </div>
+                  <div class="top_right">
+                    <div class="pro right_div">
+                      生产量<i>52</i>
+                    </div>
+                    <div class="dev right_div">
+                      发布量<i>37</i>
+                    </div>
+                  </div>
+                </div>
+                <div class="split_line"></div>
+                <div class="item_bottom">
+                  <div class="click bottom_div">
+                    <p class="num">10,230</p>
+                    <p class="tit">总点击量</p>
+                  </div>
+                  <div class="comment bottom_div">
+                    <p class="num">3,125</p>
+                    <p class="tit">总评论量</p>
+                  </div>
+                  <div class="zan bottom_div">
+                    <p class="num">7,265</p>
+                    <p class="tit">总点赞量</p>
+                  </div>
+                  <div class="share bottom_div">
+                    <p class="num">1,102</p>
+                    <p class="tit">总分享量</p>
+                  </div>
+                </div>
+              </li>
+              <li class="item">
+                <div class="item_top">
+                  <div class="touxiang">
+                    <img src="./assets/head.png"/>
+                    <p class="user_name">陈小5</p>
+                  </div>
+                  <div class="top_right">
+                    <div class="pro right_div">
+                      生产量<i>52</i>
+                    </div>
+                    <div class="dev right_div">
+                      发布量<i>37</i>
+                    </div>
+                  </div>
+                </div>
+                <div class="split_line"></div>
+                <div class="item_bottom">
+                  <div class="click bottom_div">
+                    <p class="num">10,230</p>
+                    <p class="tit">总点击量</p>
+                  </div>
+                  <div class="comment bottom_div">
+                    <p class="num">3,125</p>
+                    <p class="tit">总评论量</p>
+                  </div>
+                  <div class="zan bottom_div">
+                    <p class="num">7,265</p>
+                    <p class="tit">总点赞量</p>
+                  </div>
+                  <div class="share bottom_div">
+                    <p class="num">1,102</p>
+                    <p class="tit">总分享量</p>
+                  </div>
+                </div>
+              </li>
+            </ul>
+        </div>
+        <div id="scroll_end" class="scroll_end"></div>
+    </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'performanceAppraisal',
+  mounted () {
+    this.setFontsize('performanceAppraisal')
+    this.scrollImgLeft()
+  },
+  methods: {
+    scrollImgLeft () {
+      var speed = 30
+      var scrollBegin = document.getElementById('scroll_begin')
+      var scrollEnd = document.getElementById('scroll_end')
+      var scrollDiv = document.getElementById('scroll_div')
+      scrollEnd.innerHTML = scrollBegin.innerHTML
+      function Marquee () {
+        if (scrollEnd.offsetWidth - scrollDiv.scrollLeft <= 0) {
+          scrollDiv.scrollLeft -= scrollBegin.offsetWidth
+        } else {
+          scrollDiv.scrollLeft++
+        }
+      }
+      var MyMar = setInterval(Marquee, speed)
+      scrollDiv.οnmοuseοver = function () {
+        clearInterval(MyMar)
+      }
+      scrollDiv.οnmοuseοut = function () {
+        MyMar = setInterval(Marquee, speed)
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "~@/styles/index.scss";
+.performanceAppraisal {
+  width: 100%;
+  height: 100%;
+  padding: pxrem(58px) pxrem(50px) pxrem(53px) pxrem(50px);
+  position: relative;
+  background: #030520;
+  .performanceAppraisal-wrap {
+    width: 100%;
+    height: 100%;
+    background: url("./assets/border.png") no-repeat center;
+    background-size: 100% 100%;
+    padding: pxrem(185px) pxrem(126px) pxrem(0px);
+    color: #fff;
+    .scroll_div{
+      height: pxrem(690px);
+      overflow: hidden;
+      white-space: nowrap;
+      .scroll_begin {
+        height: pxrem(690px);
+        display: inline-block;
+        ul{
+          height: pxrem(690px);
+          display: inline-block;
+          .item{
+            width: pxrem(750px);
+            height: 100%;
+            background: url("./assets/topBack.png") no-repeat center;
+            background-size: 100% 100%;
+            box-sizing: border-box;
+            margin-right: pxrem(100px);
+            display: inline-block;
+            padding: pxrem(50px) pxrem(75px) pxrem(0px);
+            .item_top{
+              height: pxrem(190px);
+              padding-left: pxrem(50px);
+              .touxiang{
+                width: pxrem(110px);
+                height: pxrem(190px);
+                float: left;
+                img{
+                  width: pxrem(110px);
+                  height: pxrem(110px);
+                  vertical-align: middle;
+                }
+                .user_name{
+                  font-size: pxrem(32px);
+                  color: #fff;
+                  margin-top: pxrem(28px);
+                }
+              }
+              .top_right{
+                width: pxrem(320px);
+                height: pxrem(190px);
+                float: right;
+                padding-top: pxrem(20px);
+                box-sizing: border-box;
+                .right_div{
+                  // height: pxrem(50px);
+                  font-size: pxrem(34px);
+                  color: #fff;
+                  letter-spacing: pxrem(3px);
+                  // margin-right: pxrem(100px);
+                  i{
+                    font-style: normal;
+                    font-size: pxrem(48px);
+                    color: #13fee3;
+                    margin-left: pxrem(100px);
+                  }
+                }
+                .dev{
+                  margin-top: pxrem(60px);
+                }
+              }
+            }
+            .split_line{
+              height: pxrem(7px);
+              background: url('./assets/split_line.png') no-repeat;
+              background-size: 100% 100%;
+              margin-top: pxrem(57px);
+            }
+            .item_bottom{
+              margin-top: pxrem(60px);
+              // overflow: hidden;
+              .bottom_div{
+                width: 50%;
+                float: left;
+                .num{
+                  font-size: pxrem(46px);
+                  font-weight: 600;
+                  color: #00d8ff;
+                }
+                .tit{
+                  font-size: pxrem(30px);
+                  color: #fff;
+                  margin-top: pxrem(18px);
+                }
+              }
+              .zan{
+                margin-top: pxrem(65px);
+              }
+              .share{
+                margin-top: pxrem(65px);
+              }
+            }
+          }
+        }
+      }
+      .scroll_end {
+        height: pxrem(690px);
+        display: inline-block;
+        ul{
+          height: pxrem(690px);
+          display: inline-block;
+          .item{
+            width: pxrem(750px);
+            height: 100%;
+            background: url("./assets/topBack.png") no-repeat center;
+            background-size: 100% 100%;
+            box-sizing: border-box;
+            margin-right: pxrem(100px);
+            display: inline-block;
+            padding: pxrem(50px) pxrem(75px) pxrem(0px);
+            .item_top{
+              height: pxrem(190px);
+              padding-left: pxrem(50px);
+              .touxiang{
+                width: pxrem(110px);
+                height: pxrem(190px);
+                float: left;
+                img{
+                  width: pxrem(110px);
+                  height: pxrem(110px);
+                  vertical-align: middle;
+                }
+                .user_name{
+                  font-size: pxrem(32px);
+                  color: #fff;
+                  margin-top: pxrem(28px);
+                }
+              }
+              .top_right{
+                width: pxrem(320px);
+                height: pxrem(190px);
+                float: right;
+                padding-top: pxrem(20px);
+                box-sizing: border-box;
+                .right_div{
+                  // height: pxrem(50px);
+                  font-size: pxrem(34px);
+                  color: #fff;
+                  letter-spacing: pxrem(3px);
+                  // margin-right: pxrem(100px);
+                  i{
+                    font-style: normal;
+                    font-size: pxrem(48px);
+                    color: #13fee3;
+                    margin-left: pxrem(100px);
+                  }
+                }
+                .dev{
+                  margin-top: pxrem(60px);
+                }
+              }
+            }
+            .split_line{
+              height: pxrem(7px);
+              background: url('./assets/split_line.png') no-repeat;
+              background-size: 100% 100%;
+              margin-top: pxrem(57px);
+            }
+            .item_bottom{
+              margin-top: pxrem(60px);
+              // overflow: hidden;
+              .bottom_div{
+                width: 50%;
+                float: left;
+                .num{
+                  font-size: pxrem(46px);
+                  font-weight: 600;
+                  color: #00d8ff;
+                }
+                .tit{
+                  font-size: pxrem(30px);
+                  color: #fff;
+                  margin-top: pxrem(18px);
+                }
+              }
+              .zan{
+                margin-top: pxrem(65px);
+              }
+              .share{
+                margin-top: pxrem(65px);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
