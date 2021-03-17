1cc273c4919b9f8129580ef1be4c48a98b3fbc30
wanganqi <843348669@qq.com>
2021-03-17 17:53:22 +0800

    山西大屏 标题 组件

---
+++ src/components/global/common01/sxTitle/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="common01-sxtitle" id="common01-sxtitle">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sxTitle',
+  data () {
+    return {
+      titleTitle: ''
+    }
+  },
+  mounted () {
+    this.setFontsize('common01-sxtitle')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "src/styles/index.scss";
+.common01-sxtitle {
+  width: 100%;
+  height: 100%;
+  background: #0b1263 url('./assets/titlebg.png');
+  background-size: 100%;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0rem;
+  left: 0rem;
+}
+</style>
