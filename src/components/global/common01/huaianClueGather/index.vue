2283e82e50bcaea1c270d8b80e80699db1a3134b
yxn <yexinnian@hoge.cn>
2021-04-16 14:39:48 +0800

    淮安清江浦大屏

---
+++ src/components/global/common01/huaianClueGather/index.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="common01-cluegather-other">
+    <div class="cluegather-wrap common01-border">
+      <div class="common01-title" :style="setFontSize(63)">{{viewAttr.header || '线索汇聚'}}</div>
+      <div class="wrap-content">
+        <div class="list-sort">
+            <img class="imgHeight" :src='require("./assets/zixun.png")'  alt="">
+        </div>
+        <div class="item-list sys-flex sys-flex-center flex-justify-between animated" v-for="(v, k) in dataList" :key="k" :class="{'flipInX' : v.title, 'marginBottom75': customSize}" :style="{'animation-delay' : k/2+'s'}">
+          <div v-if="k == 0" class="common01-ft32 idx1">{{k+1}}</div>
+          <div v-else-if="k == 1" class="common01-ft32 idx2">{{k+1}}</div>
+          <div v-else-if="k == 2" class="common01-ft32 idx3">{{k+1}}</div>
+          <div v-else class="common01-ft32 idx4">{{k+1}}</div>
+          <div class="title common01-ft40 overhidden" :style="setFontSize(50)">{{v.title}}</div>
+          <div class="source common01-ft32" :style="setFontSize(45)">{{v.source}}</div>
+          <div class="time common01-ft32">{{v.date | dateFormat(5, 16)}}</div>
+        </div>
+      </div>
+      <div class="wrap-content">
+        <div class="list-sort">
+            <img class="imgHeight" :src='require("./assets/weixin.png")'  alt="">
+        </div>
+        <div class="item-list sys-flex sys-flex-center flex-justify-between animated" v-for="(v, k) in dataList02" :key="k" :class="{'flipInX' : v.title, 'marginBottom75': customSize}" :style="{'animation-delay' : k/2+'s'}">
+          <div v-if="k == 0" class="common01-ft32 idx1">{{k+1}}</div>
+          <div v-else-if="k == 1" class="common01-ft32 idx2">{{k+1}}</div>
+          <div v-else-if="k == 2" class="common01-ft32 idx3">{{k+1}}</div>
+          <div v-else class="common01-ft32 idx4">{{k+1}}</div>
+          <div class="title common01-ft40 overhidden" :style="setFontSize(50)">{{v.title}}</div>
+          <div class="source common01-ft32" :style="setFontSize(45)">{{v.source}}</div>
+          <div class="time common01-ft32">{{v.date | dateFormat(5, 16)}}</div>
+        </div>
+      </div>
+      <div class="wrap-content">
+        <div class="list-sort">
+            <img class="imgHeight" :src='require("./assets/weibo.png")' alt="">
+        </div>
+        <div class="item-list sys-flex sys-flex-center flex-justify-between animated" v-for="(v, k) in dataList03" :key="k" :class="{'flipInX' : v.title, 'marginBottom75': customSize}" :style="{'animation-delay' : k/2+'s'}">
+          <div v-if="k == 0" class="common01-ft32 idx1">{{k+1}}</div>
+          <div v-else-if="k == 1" class="common01-ft32 idx2">{{k+1}}</div>
+          <div v-else-if="k == 2" class="common01-ft32 idx3">{{k+1}}</div>
+          <div v-else class="common01-ft32 idx4">{{k+1}}</div>
+          <div class="title common01-ft40 overhidden" :style="setFontSize(50)">{{v.title}}</div>
+          <div class="source common01-ft32" :style="setFontSize(45)">{{v.source}}</div>
+          <div class="time common01-ft32">{{v.date | dateFormat(5, 16)}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { getHotsList } from '@/servers/interface'
+import { getDataConfig } from '@/utils/model'
+export default {
+  name: 'huaianClueGather',
+  data () {
+    return {
+      activeIdx: 0,
+      page: 1,
+      isPaging: true,
+      frequency: 15000,
+      maxPage: 3,
+      dataList: [],
+      dataList02: [],
+      dataList03: [],
+      customSize: false,
+      source: []
+    }
+  },
+  created () {
+    getDataConfig().then(res => {
+      if (Number(res.customSize)) {
+        this.customSize = true
+      }
+    })
+    const type = ['website', 'weChat', 'weiBo']
+    for (let item of type) {
+      this.getHotsList(item)
+    }
+  },
+  methods: {
+    setFontSize (size) {
+      if (this.customSize && size && size > 0) {
+        return `font-size: ${size / 100}rem!important`
+      }
+    },
+    getHotsList (type) {
+      getHotsList(type, 1, 5, this.source).then(res => {
+        if (!res.data.error_code) {
+          if (res.data.result.data.length) {
+            this.dataList = []
+            this.dataList02 = []
+            this.dataList03 = []
+            setTimeout(() => {
+              if (type === 'website') {
+                this.dataList = res.data.result.data
+              } else if (type === 'weChat') {
+                this.dataList02 = res.data.result.data
+              } else if (type === 'weiBo') {
+                this.dataList03 = res.data.result.data
+              }
+            }, 100)
+            if (this.isPaging) {
+              this.page += 1
+              if (this.page > this.maxPage) {
+                this.page = 1
+              }
+            }
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "~@/styles/index.scss";
+@import '../style/index.scss';
+.common01-cluegather-other {
+  width: 100%;
+  height: 100%;
+  padding: pxrem(40px);
+  .cluegather-wrap {
+    padding: pxrem(110px) pxrem(96px) pxrem(95px) pxrem(78px);
+    color: #fff;
+    background: url(./assets/bg02.png) no-repeat center;
+    background-size: 100% 100%;
+    .wrap-content {
+      .marginBottom75 {
+        margin-bottom: pxrem(75px)!important;
+      }
+      .list-sort{
+        text-align: center;
+        img{
+          width: 25%;
+        }
+        .imgHeight{
+          height: pxrem(94px);
+        }
+      }
+      .item-list {
+        overflow: hidden;
+        margin: pxrem(90px) 0;
+        &:last-of-type {
+          margin-bottom: pxrem(170px);
+        }
+        .title {
+          width: 60%;
+          text-align: left;
+          flex: 1;
+          margin-left: pxrem(20px);
+          font-size: 0.4rem !important;
+        }
+        .source{
+          font-size: 0.32rem !important;
+          opacity: 0.8;
+          flex: .3;
+        }
+        .time{
+          font-size: 0.32rem !important;
+          opacity: 0.8;
+        }
+        .idx1{
+            width:pxrem(56px);
+            height:pxrem(56px);
+            line-height: pxrem(56px);
+            text-align: center;
+            background:rgba(238,79,79,1);
+            border-radius:pxrem(5px);
+        }
+        .idx2{
+            width:pxrem(56px);
+            height:pxrem(56px);
+            background:rgba(230,124,39,1);
+            border-radius:pxrem(5px);
+        }
+        .idx3{
+            width:pxrem(56px);
+            height:pxrem(56px);
+            background:rgba(173,79,238,1);
+            border-radius:pxrem(5px);
+        }
+        .idx4{
+            width:pxrem(56px);
+            height:pxrem(56px);
+            background:rgba(4,102,255,1);
+            border-radius:pxrem(5px);
+        }
+        .read {
+          img {
+            width: pxrem(30px);
+            height: pxrem(30px);
+            margin-right: pxrem(18px);
+          }
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/global/common01/huaianReport/index.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="common01-report">
+    <div class="report-wrap common01-border">
+      <div class="common01-title" :style="setFontSize(63)">{{viewAttr.header || '报题展示'}}</div>
+      <div class="wrap-content">
+        <div class="item-list sys-flex sys-flex-center animated" v-for="(v, k) in dataList" :key="k" :class="{'flipInX' : v.title}" :style="{'animation-delay' : k/2+'s'}">
+          <div class="status common01-ft30" :class="{'one': v.audit_status === '0', 'two': v.audit_status === '1', 'three': v.audit_status === '2', 'four': v.audit_status === '4'}">{{v.audit_status === '0' ? '待审核' : v.audit_status === '1' ? '已通过' : v.audit_status === '2' ? '已打回' : '报审'}}</div>
+          <div class="title common01-ft40 overhidden" :style="setFontSize(50)">{{v.title}}</div>
+          <div class="name common01-ft32 overhidden" :style="setFontSize(45)">{{v.project_user_name}}</div>
+          <div class="time common01-ft32" :style="setFontSize(45)">{{v.update_time | dateFormat}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { getWorkCallReportList } from '@/servers/interface'
+import { getDataConfig } from '@/utils/model'
+export default {
+  name: 'huaianReport',
+  data () {
+    return {
+      page: 1,
+      isPaging: true,
+      frequency: 15000,
+      maxPage: 3,
+      dataList: [],
+      customSize: false
+    }
+  },
+  created () {
+    getDataConfig().then(res => {
+      if (Number(res.customSize)) {
+        this.customSize = true
+      }
+    })
+    this.getWorkCallReportList()
+    setInterval(() => {
+      this.getWorkCallReportList()
+    }, this.frequency)
+  },
+  methods: {
+    setFontSize (size) {
+      if (this.customSize && size && size > 0) {
+        return `font-size: ${size / 100}rem!important`
+      }
+    },
+    getWorkCallReportList () {
+      getWorkCallReportList(10, this.page, this.currentViewId).then((res) => {
+        if (!res.data.error_code) {
+          if (res.data.result.data.length) {
+            this.dataList = []
+            setTimeout(() => {
+              this.dataList = res.data.result.data
+            }, 100)
+            if (this.isPaging) {
+              this.page += 1
+              if (this.page > this.maxPage) {
+                this.page = 1
+              }
+            }
+          } else {
+            if (this.page !== 1) {
+              this.page = 1
+              this.getWorkCallReportList()
+            }
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import '~@/styles/index.scss';
+@import '../style/index.scss';
+.common01-report {
+  width: 100%;
+  height: 100%;
+  padding: pxrem(40px);
+  .report-wrap {
+    background: url(./assets/bg01.png) no-repeat center;
+    padding: pxrem(230px) pxrem(96px) pxrem(95px) pxrem(78px);
+    color: #fff;
+    .wrap-content {
+      width: 107%;
+      .item-list {
+        margin-bottom: pxrem(90px);
+        float: left;
+        width: 46%;
+        margin-right: pxrem(80px);
+        &:last-of-type {
+          margin-bottom: 0;
+        }
+        .status {
+          width: pxrem(120px);
+          height: pxrem(50px);
+          line-height: pxrem(50px);
+          border-radius: pxrem(2px);
+          font-size: 0.3rem !important;
+          &.one {
+            background-color: #0466FF;
+          }
+          &.two {
+            background-color: #39C78D;
+          }
+          &.three {
+            background-color: #EA4A68;
+          }
+          &.four {
+            background-color: #FF9A02;
+          }
+        }
+        .title {
+          text-align: left;
+          width: 55%;
+          margin: 0 pxrem(135px) 0 pxrem(37px);
+          font-size: 0.4rem !important;
+        }
+        .name {
+          width: 12%;
+          text-align: left;
+          opacity: 0.8;
+          font-size: 0.32rem !important;
+        }
+        .time {
+          margin-left: auto;
+          opacity: 0.8;
+          font-size: 0.32rem !important;
+        }
+      }
+    }
+  }
+}
+</style>
