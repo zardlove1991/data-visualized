e5cc95fc1cf075062b46040788aabc27bce652f9
yxn <yexinnian@hoge.cn>
2021-04-20 14:32:01 +0800

    no message

--- src/components/global/common01/huaianProject/index.vue
+++ src/components/global/common01/huaianProject/index.vue
@@ -76,7 +76,7 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import "~@/styles/index.scss";
 @import '../style/index.scss';
 .common01-project {
--- src/components/global/common01/huaianReport/index.vue
+++ src/components/global/common01/huaianReport/index.vue
@@ -71,7 +71,7 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
 @import '~@/styles/index.scss';
 @import '../style/index.scss';
 .common01-report {
